12c5012f944b7c96004aa96257867d7efa3d5839
n.malchikov <malchikov.ne@gmail.com>
2025-07-10 19:52:46 +0300

    add script

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -55,6 +55,6 @@
 <main class="flex-grow-1">
     @yield('main_content')
 </main>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
